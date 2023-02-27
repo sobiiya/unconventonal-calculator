fa5aa53b76285a48d3175490cdcec543c3fb78a0
sobiiya <ansarisobi2210@gmail.com>
2023-02-27 19:45:33 +0530

    using if statement remove code duplication

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,67 +1,71 @@
-let defaultResult=0;
-let currentResult=defaultResult;
-let logEntries=[];
+let defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-function getUserNumberInput(){
-  return  parseInt(userInput.value)
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-function creatAndWriteOutput(operator,resultBeforeCalc,calcNumber){
-    let calcDescripton=`${resultBeforeCalc} ${operator} ${calcNumber} `
-    outputResult(currentResult,calcDescripton)
+function creatAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  let calcDescripton = `${resultBeforeCalc} ${operator} ${calcNumber} `;
+  outputResult(currentResult, calcDescripton);
 }
-function writeToLog(prevNumber,operation,enteredNumber,newResult){
-    let logEntry={
-        number:prevNumber,
-        operation:operation,
-        enteredNumber:enteredNumber,
-        result:newResult
+function writeToLog(prevNumber, operation, enteredNumber, newResult) {
+  let logEntry = {
+    number: prevNumber,
+    operation: operation,
+    enteredNumber: enteredNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+function calculateResult(calculationType) {
+  let enteredNumber = getUserNumberInput();
+  if (
+    calculationType !== "ADD" &&
+    calculationType !== "SUBTRACT" &&
+    calculationType !== "MULTIPLY" &&
+    calculationType !== "DIVIDE" ||
+    !enteredNumber
+  ){
+    return
+  }
+
+  let intialResult = currentResult;
+  let mathOperator;
+
+    if (calculationType === "ADD") {
+      currentResult += enteredNumber;
+      mathOperator = "+";
+    } else if (calculationType === "SUBTRACT") {
+      currentResult -= enteredNumber;
+      mathOperator = "-";
+    } else if (calculationType === "MULTIPLY") {
+      currentResult *= enteredNumber;
+      mathOperator = "*";
+    } else if (calculationType === "DIVIDE") {
+      currentResult /= enteredNumber;
+      mathOperator = "/";
     }
-    logEntries.push(logEntry)
-    console.log(logEntries)
+  creatAndWriteOutput(mathOperator, intialResult, enteredNumber);
+  writeToLog(intialResult, calculationType, enteredNumber, currentResult);
 }
-function add(){
-    let enteredNumber=getUserNumberInput()  
-    let intialResult=currentResult
-   
-    currentResult=currentResult +enteredNumber
-    creatAndWriteOutput('+',intialResult,enteredNumber)
-    writeToLog(intialResult,'ADD',enteredNumber,currentResult)
-
-} 
-function subtract(){
-    let enteredNumber=getUserNumberInput()
-    let intialResult=currentResult
-   
-    currentResult=currentResult -enteredNumber
-    creatAndWriteOutput('-',intialResult,enteredNumber)
-    writeToLog(intialResult,'SUBTRACT',enteredNumber,currentResult)
-
+function add() {
+  calculateResult("ADD");
+}
+function subtract() {
+  calculateResult("SUBTRACT");
 }
 
-function multiply(){
-    let enteredNumber=getUserNumberInput()
-    let intialResult=currentResult
-   
-    currentResult=currentResult *enteredNumber
-    creatAndWriteOutput('*',intialResult,enteredNumber)
-    writeToLog(intialResult,'MULTIPLY',enteredNumber,currentResult)
-
-
+function multiply() {
+  calculateResult("MULTIPLY");
 }
-function divide(){
-    let enteredNumber=getUserNumberInput()
-    let intialResult=currentResult
-   
-    currentResult=currentResult /enteredNumber
-    creatAndWriteOutput('/',intialResult,enteredNumber)
-    writeToLog(intialResult,'DIVIDE',enteredNumber,currentResult)
-
-
+function divide() {
+  calculateResult("DIVIDE");
 }
 
-addBtn.addEventListener("click",add)
-subtractBtn.addEventListener("click",subtract)
-multiplyBtn.addEventListener("click",multiply)
-divideBtn.addEventListener("click",divide)
-
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
